4c57dcc382a5a3dd56a9147e47f51ab69b3e324c
zhhyang <zhhyang0404@gmail.com>
2017-09-08 18:01:57 +0800

    fix: use moment parse date

--- src/dateParser.js
+++ src/dateParser.js
@@ -1,7 +1,7 @@
 // @flow
-
+import moment from 'moment'
 export default function dateParser (date: string | number | Date): Date {
-  let parsed = new Date(date)
+  const parsed = moment(date).toDate()
   if (!Number.isNaN(parsed.valueOf())) {
     return parsed
   }
